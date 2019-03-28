56b863d240d5ac28b83b9277ce7c7e9013bf36f7
palmor357 <palmor357@gmail.com>
2019-03-28 14:34:27 -0300

    Update abredb.php

--- 2019/pPDF/abredb.php
+++ 2019/pPDF/abredb.php
@@ -1,4 +1,4 @@
 <?php
-$conexion = @mysql_connect("fdb17.royalwebhosting.net", "2351417_dbcito", "facscanto2");
-mysql_select_db("2351417_dbcito", $conexion);
+$conexion = @mysql_connect("servidor", "usuario_mysql", "contraseña_mysql");
+mysql_select_db("nombre base de datos", $conexion);
 ?>
